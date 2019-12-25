5d412aeb0cae15bed7eab76ba4ba4c371b0e3249
carefree <946715806@qq.com>
2019-12-25 18:04:24 +0800

    add version

--- Package.swift
+++ Package.swift
@@ -20,5 +20,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "Proposer", dependencies: [], path: "Proposer")
-    ]
+    ],
+    swiftLanguageVersions: [.v4_2]
 )
